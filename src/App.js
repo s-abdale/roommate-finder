7083533e144cd15ea71c56863f7380023f0c231e
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-12 13:31:21 -0500

    add header with buttons, with stores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
 
+import { Button } from './components/Button';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div>
+      <Button/>
       <Header/>
     </div>
   );
