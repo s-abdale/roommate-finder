0402832cb34d9c41d3f8508ae37cc67ed76ef0d2
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-13 14:10:52 -0500

    work on components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Button from './components/Button';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Button>LOGIN</Button>
+      <Button>SIGN UP</Button>
     </div>
   );
 }
