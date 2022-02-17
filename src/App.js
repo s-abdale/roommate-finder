50326745e8ea713f39b29435f236f0b6eaf0f90f
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-16 19:09:37 -0500

    accept incoming changes

--- src/App.js
+++ src/App.js
@@ -32,18 +32,12 @@ function App() {
             <Route path="/" element={<Main/>}></Route>
 
             <Route path="/profile" element={<Profile/>}></Route>
-<<<<<<< HEAD
-            <Route path="/" element={<Main/>}></Route>
-            <Route path="/matchList" element={<MatchList key = {users[0]} users = {users[0]}/>}></Route>
-
-=======
             <Route path="/preferences" element={<Preferences/>}></Route>
             <Route path="/editProfile" element={<EditProfile/>}></Route>
 
             <Route path="/likes" element={<Likes key = {users[0]} users = {users[0]}/>}></Route>
 
             <Route path="/matchList" element={<MatchList key = {users[0]} users = {users[0]}/>}></Route>
->>>>>>> feature/card-swiper
         </Routes>
       </Router>
 
