6dec4c0ae2d7a0fb6e2c35594b5a98e331c2b6b2
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-18 14:26:58 -0500

    update axios for preferences

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -5,10 +5,23 @@ import './preferences.css';
 
 export default function Preferences() {
 
-  const [male, setMale] = useState('t');
-  const [female, setFemale] = useState('t');
-  const [other, setOther] = useState('t');
-  const [petFriendly, setPetFriendly] = useState('f');
+  const [male, setMale] = useState('');
+  const [female, setFemale] = useState('');
+  const [other, setOther] = useState('');
+  const [petFriendly, setPetFriendly] = useState('');
+
+  useEffect(() => {
+    axios.get(`/api/user/preferences`, {
+    })
+    .then(response => {
+      const preferencesData = response.data.preferences[0];
+      console.log(preferencesData);
+      setMale(preferencesData.male)
+      setFemale(preferencesData.female)
+      setOther(preferencesData.other)
+      setPetFriendly(preferencesData.pet_friendly)
+    })
+  }, [])
 
   const savePref = () => {
     console.log("male:", male)
@@ -41,35 +54,35 @@ export default function Preferences() {
       <form onSubmit={event => event.preventDefault()} autoComplete="off" className='preferences'>
         <h1>Preferences</h1>
         <label>Male:
-         <input type="text"  placeholder="Male: true" list="browsePref" name="Pref" onChange={(e) => {setMale(e.target.value)}}/>    
+         <input type="text"  value={male} list="browseMale"  name="Pref" onChange={(e) => {setMale(e.target.value)}}/>    
         </label>
-        <datalist id="browsePref">
-            <option value="t"/> 
-            <option value="f"/> 
+        <datalist id="browseMale">
+            <option value="true"/> 
+            <option value="false"/> 
         </datalist>
 
         <label>Female:
-         <input type="text"  placeholder="female: true" list="browsePref" name="Pref" onChange={(e) => {setFemale(e.target.value)}}/>    
+         <input type="text"  value={female} list="browseFemale" name="Pref" onChange={(e) => {setFemale(e.target.value)}}/>    
         </label>
-        <datalist id="browsePref">
-            <option value="t"/> 
-            <option value="f"/> 
+        <datalist id="browseFemale">
+            <option value="true"/> 
+            <option value="false"/> 
         </datalist>
 
         <label>Other:
-         <input type="text"  placeholder="other: false" list="browsePref" name="Pref" onChange={(e) => {setOther(e.target.value)}}/>    
+         <input type="text" value={other}  list="browseOther" name="Pref" onChange={(e) => {setOther(e.target.value)}}/>    
         </label>
-        <datalist id="browsePref">
-            <option value="t"/> 
-            <option value="f"/> 
+        <datalist id="browseOther">
+            <option value="true"/> 
+            <option value="false"/> 
         </datalist>
 
         <label>Pet Friendly:
-         <input type="text"  placeholder="Pet friendly: false" list="browsePref" name="Pref" onChange={(e) => {setPetFriendly(e.target.value)}}/>    
+         <input type="text" value={petFriendly} list="browsePet" name="Pref" onChange={(e) => {setPetFriendly(e.target.value)}}/>    
         </label>
-        <datalist id="browsePref">
-            <option value="t"/> 
-            <option value="f"/> 
+        <datalist id="browsePet">
+            <option value="true"/> 
+            <option value="false"/> 
         </datalist>
         <button type='submit' onClick={savePref}> Save Preferences </button>
       </form>
