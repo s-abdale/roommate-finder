d1321383899636eee8d32acd82c4128c8c0459dc
Safia Abdale <safia.abdale@gmail.com>
2022-02-19 08:31:37 -0500

    Added styling to individual cards in likes page

--- src/components/SimpleProfile.jsx
+++ src/components/SimpleProfile.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './detailedProfile.css';
+// import './detailedProfile.css';
+import './simpleProfile.css';
 import {useParams, useNavigate} from 'react-router-dom';
+import Card from '@mui/material/Card';
+
 
 
 
@@ -10,37 +13,17 @@ export default function SimpleProfile(props) {
 
 
   return (
-    <section className='detailed-profile'>
+    <div className='card-wrapper'>
 
+      <Card className='simple-profile'>
 
+        <article className='user-details'>
 
-      {/* square image*/}
-
-
-
-
-      <article className='user-details'>
-        {/* works - METHOD 1 */}
-
-        <p>{props.user_name}</p>
-        <p>{props.bio}</p>
-
-        <br/>
-        
-
-
-        {/* works - METHOD 2 */}
-        {/* <span>{users.first_name} {users.last_name}</span>
-
-        <p>{users.location}</p>
-        <p>{users.bio}</p>
-
-        <br/>
-        
-        <p>CONTACT INFO:</p>
-        <p>{users.email}</p>
-        <p>Tel.: {users.phone_number}</p> */}
-      </article>
-    </section>
+          <p>{props.user_name}</p>
+          <p>{props.bio}</p>
+          
+        </article>
+      </Card>
+    </div>
   );
 };
