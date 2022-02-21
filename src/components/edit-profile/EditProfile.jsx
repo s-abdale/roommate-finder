d1ee5b206bb2def34725e6260e85e9ae7364d26c
Safia Abdale <safia.abdale@gmail.com>
2022-02-20 23:45:00 -0500

    Fixed form for profile edit

--- src/components/edit-profile/EditProfile.jsx
+++ src/components/edit-profile/EditProfile.jsx
@@ -2,6 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import {BrowserRouter as Router, Link , Route, Routes, useNavigate} from 'react-router-dom';
 import '../profile.css';
+import './editProfile.css'
+
+
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Radio from '@mui/material/Radio';
+
+
 // import {useNavigate} from 'react-router-dom';
 
 
@@ -36,11 +51,13 @@ export default function EditProfile(props){
   const edit = () => {
     // process gender
     let isMale = null
-    if (genderEdit === "Female") {
+    if (genderEdit === false) {
       isMale = false
-    } else if (genderEdit === "Male") {
+    } else if (genderEdit === true) {
       isMale = true
     }
+
+
     // post new results to db
     axios.post("/api/user/profile", {
       first_name: firstnameEdit,
@@ -82,90 +99,146 @@ export default function EditProfile(props){
     })
   }, [])
 
-/*
-{"profile":[{"first_name":"Mariah","last_name":"Carey","user_name":"MCarey","phone_number":"1020304050","email":"mcarey@gmail.com","contact_info":"@mariahCarey","user_image":"https://i.imgur.com/eBffHEo.jpeg","bio":"this is a bio","location":"Beverly Hills","gender":false}]}
-*/
+
+  let translatedGenderEdit;
+  if (genderEdit == false) {
+    translatedGenderEdit = "Female"
+  } else if (genderEdit == true) {
+    translatedGenderEdit = "Male"
+  } else if (translatedGenderEdit == null) {
+    translatedGenderEdit = "They/Them"
+  }
+
 
 
 
   return (
     <section className='main-body'>
-      <h3 className='page-header'>EDIT PROFILE</h3>
 
       <form onSubmit={event => event.preventDefault()} autoComplete="off" className='profile-text'>
 
-        <label>First Name</label>
-        <input type='text' value={firstnameEdit}name="firstnameEdit" onChange={(e) => {setFirstnameEdit(e.target.value)}}></input>
-        <br></br>
+        <TextField
+          id="outlined-basic"
+          label="First Name"
+          variant="outlined"
+          margin="normal"
+          type='text' value={firstnameEdit}
+          name="firstnameEdit"
+          onChange={(e) => {setFirstnameEdit(e.target.value)}}
+        />
+        <br/>
 
-        <label>Last Name</label>
-        <input type='text'  value={lastnameEdit} name="lastnameEdit"onChange={(e) => {setLastnameEdit(e.target.value)}} ></input>
-        <br></br>
-
-        {/* <label>User Name</label>
-        <input type='text' placeholder='Username' name="usernameEdit" onChange={(e) => {setUsernameEdit(e.target.value)}} ></input>
-        <br></br> */}
-
-        <label>Email</label>
-        <input type='email' value={emailEdit} name="emailEdit" onChange={(e) => {setEmailEdit(e.target.value)}}></input>
-        <br></br>
-
-        
-        <label>Phone Number</label>
-        <input type='text' value={phonenumberEdit}  name="phonenumberEdit" onChange={(e) => {setPhonenumberEdit(e.target.value)}} ></input>
-        <br></br>
-
-
-        {/* change to radio? */}
-        <label>Gender:
-          <input type="text" value={genderEdit}  placeholder="Gender" list="browsers" name="genderEdit" onChange={(e) => {setGenderEdit(e.target.value)}}/>    
-        </label>
-          <datalist id="browsers">
-            <option value="Male"/> 
-            <option value="Female"/> 
-            <option value="They/Them"/> 
-            {/* <option value="Prefer not to say"/> */}
-          </datalist>
-          <br></br>
-
-          {/* <label>Gender</label>
-          <input type='text' placeholder='Select Gender' onChange={(e) => {setGenderEdit(e.target.value)}} ></input>
-          <br></br> */}
-
-          <label>Contact Info</label>
-          <input type='text' value={contactinfoEdit} name="contactinfoEdit" onChange={(e) => {setContactinfoEdit(e.target.value)}}></input>
-          <br></br>
+        <TextField
+          id="outlined-basic"
+          label="Last Name"
+          variant="outlined"
+          margin="normal"
+          type='text'  
+          value={lastnameEdit} 
+          name="lastnameEdit"onChange={(e) => {setLastnameEdit(e.target.value)}} 
+        />
+        <br/>
 
 
-          <label>Bio</label>
-          <input type='text' value={bioEdit}  name="bioEdit" onChange={(e) => {setBioEdit(e.target.value)}}></input>
-          <br></br>
-
-          /* ---------------------------------------- */
-
-          <label>location</label>
-          <input value={locationEdit}  name="locationEdit" onChange={(e) => {setLocationEdit(e.target.value)}}></input>
-          <br></br>
-
-          <label>image</label>
-          <input value={`${userimageEdit}`}  name="userimageEdit" onChange={(e) => {setUserimageEdit(e.target.value)}}></input>
-          <br></br>
-
-          <Link to="/profile">
-            <button type='submit' onClick={edit}> Update </button>
-          </Link>
-          
-
-
-
-
-
-
-        </form>
-
-
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          margin="normal"
+          type='email' 
+          value={emailEdit} 
+          name="emailEdit" 
+          onChange={(e) => {setEmailEdit(e.target.value)}}
+        />
+        <br/>
 
         
+        <TextField
+          id="outlined-basic"
+          label="Phone Number"
+          variant="outlined"
+          margin="normal"
+          type='text' 
+          value={phonenumberEdit}  
+          name="phonenumberEdit" 
+          onChange={(e) => {setPhonenumberEdit(e.target.value)}} 
+        />
+        <br/>
+
+
+        <FormControl fullWidth>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+
+          <RadioGroup
+            label="Gender"
+            aria-labelledby="demo-radio-buttons-group-label"
+            value={translatedGenderEdit}
+            name="genderEdit"
+            onChange={(e) => {setGenderEdit(e.target.value)}}
+          >
+            <FormControlLabel value={"Male"} control={<Radio />} label="Male"/>
+            <FormControlLabel value={"Female"} control={<Radio />} label="Female"/>
+            <FormControlLabel value={"They/Them"} control={<Radio />} label="They/Them"/>
+          </RadioGroup>
+        </FormControl>
+
+
+        <TextField
+          id="outlined-basic"
+          label="Contact Info"
+          variant="outlined"
+          margin="normal"
+          type='text' 
+          value={contactinfoEdit} 
+          name="contactinfoEdit" 
+          onChange={(e) => {setContactinfoEdit(e.target.value)}}
+        />
+        <br/>
+
+
+        <TextField
+          id="outlined-basic"
+          label="Bio"
+          variant="outlined"
+          margin="normal"
+          multiline="true"
+          type='text' 
+          value={bioEdit}  
+          name="bioEdit" 
+          onChange={(e) => {setBioEdit(e.target.value)}}
+        />
+        <br/>
+
+
+        <TextField
+          id="outlined-basic"
+          label="Location"
+          variant="outlined"
+          margin="normal"
+          value={locationEdit}  
+          name="locationEdit" 
+          onChange={(e) => {setLocationEdit(e.target.value)}}
+        />
+        <br/>
+
+
+        <TextField
+          id="outlined-basic"
+          label="Profile Picture"
+          variant="outlined"
+          margin="normal"
+          value={`${userimageEdit}`}  
+          name="userimageEdit" 
+          onChange={(e) => {setUserimageEdit(e.target.value)}}
+        />
+        <br/>
+
+
+        <Link to="/profile">
+          <button type='submit' onClick={edit}> Update </button>
+        </Link>
+
+      </form>   
     </section>
   );
 };
