706f9f5760cbd764320d63a4d5e1983f52c4d617
Safia Abdale <safia.abdale@gmail.com>
2022-02-24 13:21:57 -0500

    Added function to process bio

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -16,6 +16,7 @@ import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import PhoneIphoneRoundedIcon from '@mui/icons-material/PhoneIphoneRounded';
 import PersonPinOutlinedIcon from '@mui/icons-material/PersonPinOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 export default function Profile(props) {
   
@@ -39,7 +40,7 @@ export default function Profile(props) {
     .then(response => {
       //Name, gender, password, should not be updated? 
       const profileData = response.data.profile[0]
-      console.log(profileData)
+      // console.log(profileData)
       setUser(profileData)
       // setFirstnameReg(profileData.first_name)
       // setLastnameReg(profileData.last_name)
@@ -56,13 +57,48 @@ export default function Profile(props) {
   }, [])
 
   function formatPhoneNumber(phoneNumberString) {
+    // console.log(phoneNumberString);
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
+    // console.log(cleaned);
     var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+    // console.log(match);
     if (match) {
       return '(' + match[1] + ')-' + match[2] + '-' + match[3]
     }
     return null
   }
+
+  
+  // get first emoji (housing tag) from bio
+  function formatBioGetEmoji(bioString) {    
+    if (bioString !== undefined) {
+      const regex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+      // console.log(bioString);
+      const found = bioString.match(regex);
+      console.log(found[0]);
+      return found[0]
+    } else {
+      console.log(`couldn't find an emoji :(`)
+    }
+  }
+  const firstEmoji = formatBioGetEmoji(user.bio);
+  
+  // get bio minus emoji (tag)
+  function formatBioGetBio(bioString) {
+    if (bioString !== undefined) {
+      const regex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+      const remaining = bioString.replace(regex, '');
+      // console.log(remaining);
+      return remaining
+    } else {
+      console.log(`couldn't find a string :(`)
+    }
+  }
+  const restOfBio = formatBioGetBio(user.bio);
+
+
+
+
 
   return (
     <section className='main-body-profile'>
@@ -83,8 +119,13 @@ export default function Profile(props) {
           </tr>
 
           <tr>
+            <td><HomeOutlinedIcon className='icons-detailedProfile' color="disabled" /></td>
+            <td>{firstEmoji}</td>
+          </tr>
+
+          <tr>
             <td><PersonPinOutlinedIcon className='icons-detailedProfile' color="disabled"/></td>
-            <td>{user.bio}</td>
+            <td>{restOfBio}</td>
           </tr>
 
           <tr>
